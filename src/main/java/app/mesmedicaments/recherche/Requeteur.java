9bdb4d61e016f945a4fb31b2cdad417a2a3314ef
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2020-01-02 19:03:33 +0100

    Utilise des opérateurs pour rechercher les préfixes

--- src/main/java/app/mesmedicaments/recherche/Requeteur.java
+++ src/main/java/app/mesmedicaments/recherche/Requeteur.java
@@ -1,7 +1,12 @@
 package app.mesmedicaments.recherche;
 
 import java.io.IOException;
+import java.util.Set;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.Sets;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -10,6 +15,8 @@ import app.mesmedicaments.JSONObjectUneCle;
 
 public class Requeteur {
 
+    private static final String[] caracteresAEchapper = "\\ + - && || ! ( ) { } [ ] ^ \" ~ * ? : /".split(" ");
+
     private final Logger logger;
 
     public Requeteur(Logger logger) {
@@ -17,11 +24,43 @@ public class Requeteur {
     }
 
     public JSONArray rechercher(String recherche) throws IOException {
-        final JSONObject requete = new JSONObjectUneCle("search", recherche)
+        final String rechercheFormatee = separerTermes(echapperCaracteresSpeciaux(recherche))
+            .stream()
+            .map(terme -> construireSousRequete(terme))
+            .collect(Collectors.joining("&&"));
+        final JSONObject requete = new JSONObjectUneCle("search", rechercheFormatee)
             .put("searchMode", "all")
+            .put("queryType", "full")
             .put("top", 30);
         final JSONArray resultats = new SearchClient(logger).queryDocuments(requete);
         return resultats;
+    }
+
+    private Set<String> separerTermes(String recherche) {
+        return Sets.newHashSet(recherche.split(" ", 0))
+            .stream()
+            .filter(t -> !t.matches(" *"))
+            .collect(Collectors.toSet());
+    }
+
+    private String construireSousRequete(String terme) {
+        String sousRequete = "(";
+        //sousRequete += terme + "~||";
+        sousRequete += terme + "* || ";
+        sousRequete += terme + ")";
+        return sousRequete;
+    }
+
+    private String echapperCaracteresSpeciaux(String recherche) {
+        for (String caractere : caracteresAEchapper) {
+            String quotedCaractere = Pattern.quote(caractere);
+            recherche = recherche.replaceAll(
+                String.format("(%s)", quotedCaractere),
+                "\\\\$1"
+                
+            );
+        }
+        return recherche;
     }
 
 }

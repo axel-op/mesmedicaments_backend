4be5808086c96d33ab7ec6695ae4ba79c7cbb8d7
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2020-01-13 02:36:49 +0100

    Ajoute la fonction Reveil

---
+++ src/main/java/app/mesmedicaments/api/privee/Reveil.java
@@ -0,0 +1,19 @@
+package app.mesmedicaments.api.privee;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
+
+public final class Reveil {
+
+    @FunctionName("reveil")
+    public void reveil(
+        @TimerTrigger(name = "reveilTrigger", schedule = "0 */15 * * * *")
+        final String timerInfo,
+        final ExecutionContext context
+    ) throws InterruptedException {
+        Thread.sleep(2500);
+    }
+
+
+}

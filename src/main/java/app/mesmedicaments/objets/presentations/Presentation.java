e981aacaacf39e8a115f10f40451fb5392841622
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2020-04-02 17:23:31 +0200

    Suppression du bundle d'extension

--- src/main/java/app/mesmedicaments/objets/presentations/Presentation.java
+++ src/main/java/app/mesmedicaments/objets/presentations/Presentation.java
@@ -1,8 +1,7 @@
 package app.mesmedicaments.objets.presentations;
 
-import org.json.JSONString;
-
 import app.mesmedicaments.IJSONSerializable;
 import app.mesmedicaments.objets.Pays;
 
-public abstract class Presentation<P extends Pays> implements IJSONSerializable, JSONString {}
+public abstract class Presentation<P extends Pays> implements IJSONSerializable {
+}

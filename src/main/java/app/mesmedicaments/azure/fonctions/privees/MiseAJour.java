2c7185870241119b0628a93610b0ff0145fe2846
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2020-04-10 02:16:02 +0200

    Active la mise à jour auto des médicaments

--- src/main/java/app/mesmedicaments/azure/fonctions/privees/MiseAJour.java
+++ src/main/java/app/mesmedicaments/azure/fonctions/privees/MiseAJour.java
@@ -23,6 +23,7 @@ import app.mesmedicaments.misesajour.updaters.UpdaterFrance;
 import app.mesmedicaments.objets.ClasseSubstances;
 import app.mesmedicaments.objets.Interaction;
 import app.mesmedicaments.objets.Pays;
+import app.mesmedicaments.objets.Pays.France;
 import app.mesmedicaments.objets.medicaments.MedicamentFrance;
 import app.mesmedicaments.objets.presentations.PresentationFrance;
 import app.mesmedicaments.objets.substances.Substance;
@@ -35,10 +36,17 @@ public class MiseAJour {
     static public void main(String[] args) throws Exception {
         logger.setLevel(Level.ALL);
         final ClientTableDatesMaj clientDates = new ClientTableDatesMaj();
-        final Set<Substance<Pays.France>> nouvellesSubstances = updateBDPM();
+        final Updater<France, Substance<France>, PresentationFrance, MedicamentFrance> updater = new UpdaterFrance(logger);
+        final Set<Substance<France>> substances = updateAndGetSubstances(updater);
+        final Set<MedicamentFrance> medicaments = updateAndGetMedicaments(updater);
+        indexMedicaments(medicaments);
         clientDates.setDateMajBDPM(LocalDate.now());
+
         // ignoré car inutile pour le moment
         // updateClassesSubstances(nouvellesSubstances);
+
+        // ignoré car il y a un bug
+        /*
         final Set<Interaction> nouvellesInteractions = getNouvellesInteractions();
         logger.info(nouvellesInteractions.size() + " interactions créées");
         new ClientTableInteractions()
@@ -54,37 +62,41 @@ public class MiseAJour {
                 .collect(Collectors.toSet())
             );
         clientDates.setDateMajInteractions(LocalDate.now());
+        */
+        logger.info("Mise à jour terminée !");
     }
 
-    /**
-     * Renvoie les nouvelles substances car il y en aura besoin pour la suite.
-     * @return
-     * @throws ExceptionTable
-     * @throws IOException
-     */
-    static private Set<Substance<Pays.France>> updateBDPM() throws ExceptionTable, IOException {
-        final Updater<Pays.France, Substance<Pays.France>, PresentationFrance, MedicamentFrance> updater = new UpdaterFrance(logger);
+    static private Set<Substance<France>> updateAndGetSubstances(
+        Updater<France, Substance<France>, PresentationFrance, MedicamentFrance> updater
+    ) throws ExceptionTable, IOException {
         logger.info("Mise à jour des substances...");
-        final Set<Substance<Pays.France>> nouvellesSubstances = updater.getNouvellesSubstances();
-        logger.info(
-                String.valueOf(nouvellesSubstances.size())
-                        + " substances récupérées vont être mises à jour...");
-        new ClientTableSubstances().set(nouvellesSubstances);
+        final Set<Substance<Pays.France>> substances = updater.getNouvellesSubstances();
+        logger.info(substances.size() + " substances récupérées vont être mises à jour...");
+        new ClientTableSubstances().set(substances);
+        return substances;
+    }
+
+    static private Set<MedicamentFrance> updateAndGetMedicaments(
+        Updater<France, Substance<France>, PresentationFrance, MedicamentFrance> updater
+    ) throws ExceptionTable, IOException {
         logger.info("Mise à jour des médicaments...");
-        final Set<MedicamentIncomplet<Pays.France, MedicamentFrance>> medicamentsIncomplets = updater.getNouveauxMedicaments();
-        logger.info(String.valueOf(medicamentsIncomplets.size()) + " médicaments récupérés");
+        final Set<MedicamentIncomplet<France, MedicamentFrance>> medsIncomplets = updater.getNouveauxMedicaments();
+        logger.info(medsIncomplets.size() + " médicaments récupérés");
         logger.info("Récupération de leurs effets indésirables...");
-        final Set<MedicamentFrance> nouveauxMedicaments = medicamentsIncomplets
+        final Set<MedicamentFrance> medicaments = medsIncomplets
                 .parallelStream()
                 .map(MedicamentIncomplet::getMedicamentComplet)
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .collect(Collectors.toSet());
         logger.info("Ces médicaments vont maintenant être mis à jour...");
-        new ClientTableMedicamentsFrance().set(nouveauxMedicaments);
+        new ClientTableMedicamentsFrance().set(medicaments);
+        return medicaments;
+    }
+
+    static private void indexMedicaments(Set<MedicamentFrance> medicaments) throws IOException {
         logger.info("Ces médicaments vont maintenant être indexés...");
-        new ClientRecherche(logger).index(nouveauxMedicaments);
-        return nouvellesSubstances;
+        new ClientRecherche(logger).index(medicaments);
     }
 
     static private void updateClassesSubstances(Set<? extends Substance<?>> nouvellesSubstances)

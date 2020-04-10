f743e8b04e18941e89ddce3a0de311a6df4edf99
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2020-04-10 22:11:25 +0200

    Supprime la dépendance au SDK Azure complet
    * Utilise l'API REST pour déployer les conteneurs de mise à jour.
    * + de 30 Mo économisés après compilation

--- src/main/java/app/mesmedicaments/azure/fonctions/privees/DeploiementConteneur.java
+++ src/main/java/app/mesmedicaments/azure/fonctions/privees/DeploiementConteneur.java
@@ -1,57 +1,134 @@
 package app.mesmedicaments.azure.fonctions.privees;
 
-import java.util.stream.Collectors;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map.Entry;
+import java.util.logging.Logger;
 
-import com.microsoft.azure.AzureEnvironment;
-import com.microsoft.azure.credentials.AppServiceMSICredentials;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.TimerTrigger;
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.containerinstance.ContainerGroupRestartPolicy;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import app.mesmedicaments.Environnement;
+import app.mesmedicaments.utils.ClientHttp;
+import app.mesmedicaments.utils.MultiMap;
+import app.mesmedicaments.utils.Utils;
 
 public
 class DeploiementConteneur {
 
     @FunctionName("containerDeployment")
     public void containerDeployment(
-        @TimerTrigger(name = "containerDeploymentTrigger", schedule = "0 0 0 1,15 * *")
+        @TimerTrigger(
+            name = "containerDeploymentTrigger", 
+            schedule = "0 0 0 1,15 * *")
         final String timerInfo,
         final ExecutionContext context
     ) {
-        final AppServiceMSICredentials credentials = new AppServiceMSICredentials(AzureEnvironment.AZURE);
-        final Azure azure = Azure.authenticate(credentials)
-            .withSubscription(Environnement.AZURE_SUBSCRIPTIONID);
-        final String aciName = "maintenance";
-        azure.containerGroups()
-            .define(aciName)
-            .withRegion(Region.EUROPE_WEST)
-            .withExistingResourceGroup(Environnement.AZURE_RESOURCEGROUP)
-            .withLinux()
-            .withPrivateImageRegistry(
-                "index.docker.io", 
-                Environnement.MAINTENANCE_DOCKERUSERNAME, 
-                Environnement.MAINTENANCE_DOCKERTOKEN)
-            .withoutVolume()
-            .defineContainerInstance(aciName)
-                .withImage(Environnement.MAINTENANCE_IMAGE)
-                .withoutPorts()
-                .withEnvironmentVariableWithSecuredValue(System.getenv()
-                                                                .entrySet()
-                                                                .stream()
-                                                                .filter(e -> e.getKey()
-                                                                                .matches("[A-z_]+"))
-                                                                .collect(Collectors.toMap(
-                                                                    e -> e.getKey(),
-                                                                    e -> e.getValue())))
-            .attach()
-            .withRestartPolicy(ContainerGroupRestartPolicy.NEVER)
-            .withLogAnalytics(
-                Environnement.MAINTENANCE_LOGANALYTICSID,
-                Environnement.MAINTENANCE_LOGANALYTICSKEY)
-            .create();
+        final Logger logger = context.getLogger();
+        try {
+            final String containerName = "maintenance";
+            final String accessToken = getAccessToken();
+            final ClientHttp httpClient = new ClientHttp(logger);
+            final MultiMap<String, String> requestProperties = new MultiMap<>();
+            requestProperties.add("Authorization", "Bearer " + accessToken);
+
+            // https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/
+            // {resourceGroupName}/providers/Microsoft.ContainerInstance/containerGroups/
+            // {containerGroupName}
+            final String urlBase = "https://management.azure.com/subscriptions/"
+                                    + Environnement.AZURE_SUBSCRIPTIONID + "/"
+                                    + "resourceGroups/"
+                                    + Environnement.AZURE_RESOURCEGROUP + "/"
+                                    + "providers/Microsoft.ContainerInstance/containerGroups/"
+                                    + containerName;
+
+            // CREATE OR UPDATE
+            // PUT {urlBase}?api-version=2018-10-01"
+            final String urlCreate = urlBase + "?api-version=2018-10-01";
+
+            final JSONArray envVariables = new JSONArray();
+            for (Entry<String, String> e : System.getenv().entrySet()) {
+                if (e.getKey().matches("[A-z_]+")) {
+                    envVariables.put(new JSONObject()
+                                    .put("name", e.getKey())
+                                    .put("secureValue", e.getValue()));
+                }
+            }
+
+            final JSONObject resources = new JSONObject()
+                .put("requests", new JSONObject()
+                    .put("memoryInGB", 1.5)
+                    .put("cpu", 1));
+            
+            final JSONObject container = new JSONObject()
+                .put("name", containerName)
+                .put("properties", new JSONObject()
+                    .put("image", Environnement.MAINTENANCE_IMAGE)
+                    .put("environmentVariables", envVariables)
+                    .put("resources", resources));
+
+            final JSONArray containers = new JSONArray()
+                .put(container);
+
+            final JSONArray regCredentials = new JSONArray()
+                .put(new JSONObject()
+                    .put("server", "index.docker.io")
+                    .put("username", Environnement.MAINTENANCE_DOCKERUSERNAME)
+                    .put("password", Environnement.MAINTENANCE_DOCKERTOKEN));
+            
+            final JSONObject diagnostics = new JSONObject()
+                .put("logAnalytics", new JSONObject()
+                    .put("workspaceId", Environnement.MAINTENANCE_LOGANALYTICSID)
+                    .put("workspaceKey", Environnement.MAINTENANCE_LOGANALYTICSKEY)
+                    .put("logType", "ContainerInsights"));
+
+            final JSONObject properties = new JSONObject()
+                .put("containers", containers)
+                .put("imageRegistryCredentials", regCredentials)
+                .put("diagnostics", diagnostics)
+                .put("osType", "Linux")
+                .put("restartPolicy", "Never");
+
+            final JSONObject requestBody = new JSONObject()
+                .put("location", "westeurope")
+                .put("properties", properties);
+
+            final MultiMap<String, String> headersCreate = new MultiMap<>(requestProperties);
+            headersCreate.add("Content-Type", "application/json");
+            final InputStream isCreate = httpClient.send("PUT", urlCreate, headersCreate, requestBody.toString());
+            logger.info(Utils.stringify(new InputStreamReader(isCreate)));
+
+            // START
+            // POST {urlBase}/start?api-version=2018-10-01
+            final String urlStart = urlBase + "/start?api-version=2018-10-01";
+            final MultiMap<String, String> headersStart = new MultiMap<>(requestProperties);
+            final InputStream isStart = httpClient.post(urlStart, headersStart, null);
+            logger.info(Utils.stringify(new InputStreamReader(isStart)));            
+            
+        } catch (IOException e) {
+            Utils.logErreur(e, logger);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getAccessToken() throws IOException {
+        String url = System.getenv("MSI_ENDPOINT");
+        final String secret = System.getenv("MSI_SECRET");
+        if (url == null || secret == null)
+            throw new RuntimeException("Cannot find environment variable "
+                                        + url == null ? "MSI_ENDPOINT" : "MSI_SECRET");
+        url += "?resource=https://management.azure.com&api-version=2017-09-01";
+        final MultiMap<String, String> headers = new MultiMap<>();
+        headers.add("Content-Type", "application/json");
+        headers.add("Secret", secret);
+        final ClientHttp client = new ClientHttp();
+        final InputStream is = client.get(url, headers);
+        final JSONObject response = new JSONObject(Utils.stringify(new InputStreamReader(is)));
+        return response.getString("access_token");
     }
 }

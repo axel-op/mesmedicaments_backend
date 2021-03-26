d8374fed31a723bea8464fe0963041cde7de92d2
Axel Ogereau-Peltier <49279289+axel-op@users.noreply.github.com>
2021-03-26 22:35:44 +0100

    Utilise ma bibliothèque pour la connexion au DMP

--- src/main/java/app/mesmedicaments/azure/fonctions/publiques/Connexion.java
+++ src/main/java/app/mesmedicaments/azure/fonctions/publiques/Connexion.java
@@ -1,6 +1,5 @@
 package app.mesmedicaments.azure.fonctions.publiques;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.logging.Logger;
 
@@ -16,60 +15,47 @@ import com.microsoft.azure.functions.annotation.HttpTrigger;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import app.mesmedicaments.dmp.Authentificateur;
-import app.mesmedicaments.dmp.DonneesConnexion;
-import app.mesmedicaments.dmp.Authentificateur.ReponseConnexion;
-import app.mesmedicaments.dmp.Authentificateur.ReponseConnexion1;
-import app.mesmedicaments.dmp.Authentificateur.ReponseConnexion2;
+import app.mesmedicaments.dmp.DMPTimeoutException;
+import app.mesmedicaments.dmp.authentication.DMPAuthenticationException;
+import app.mesmedicaments.dmp.authentication.DMPFirstAuthenticationPage;
+import app.mesmedicaments.dmp.authentication.DMPThirdAuthenticationPage;
 import app.mesmedicaments.utils.Utils;
 
 public final class Connexion {
 
     @FunctionName("connexion")
-    public HttpResponseMessage connexion(
-        @HttpTrigger(name = "connexionTrigger", 
-            authLevel = AuthorizationLevel.ANONYMOUS, 
-            methods = { HttpMethod.POST }, 
-            dataType = "string", 
-            route = "connexion/{etape:int}")
-        final HttpRequestMessage<Optional<String>> request,
-        @BindingName("etape")
-        final int etape,
-        final ExecutionContext context
-    ) {
+    public HttpResponseMessage connexion(@HttpTrigger(name = "connexionTrigger",
+            authLevel = AuthorizationLevel.ANONYMOUS, methods = {HttpMethod.POST},
+            dataType = "string",
+            route = "connexion/{etape:int}") final HttpRequestMessage<Optional<String>> request,
+            @BindingName("etape") final int etape, final ExecutionContext context) {
         final JSONObject corpsReponse = new JSONObject();
         final Logger logger = context.getLogger();
-        HttpStatus codeHttp = HttpStatus.NOT_IMPLEMENTED;
+        HttpStatus codeHttp = HttpStatus.OK;
         try {
             final JSONObject corpsRequete = new JSONObject(request.getBody().get());
             final String id = corpsRequete.getString("id");
-            final Authentificateur auth = new Authentificateur(logger, id);
             switch (etape) {
                 case 1:
                     final String mdp = corpsRequete.getString("mdp");
-                    final ReponseConnexion1 resultat = auth.connexionDMPPremiereEtape(mdp);
-                    corpsReponse.put("donneesConnexion", resultat.donneesConnexion);
-                    codeHttp = obtenirCodeHttp(resultat);
-                    if (codeHttp == HttpStatus.OK) {
-                        corpsReponse.put("envoiCode", resultat.modeEnvoiCode);
-                    }
+                    final var page = DMPFirstAuthenticationPage.create().provideCredentials(id, mdp)
+                            .sendOTPCode();
+                    corpsReponse.put("donneesConnexion", page.toJSONObject());
+                    corpsReponse.put("envoiCode", "SMS ou email");
                     break;
                 case 2:
-                    final String code = String.valueOf(corpsRequete.getInt("code"));
-                    final DonneesConnexion donneesConnexion = new DonneesConnexion(corpsRequete.getJSONObject("donneesConnexion"));
-                    final ReponseConnexion2 resultat2 = auth.connexionDMPDeuxiemeEtape(code, donneesConnexion);
-                    corpsReponse.put("donneesConnexion", resultat2.donneesConnexion);
-                    codeHttp = obtenirCodeHttp(resultat2);
-                    if (codeHttp == HttpStatus.OK) {
-                        corpsReponse.put("urlRemboursements", resultat2.urlRemboursements);
-                        if (resultat2.genre != null) corpsReponse.put("genre", resultat2.genre);
-                    }
+                    final var code = corpsRequete.getInt("code");
+                    final var pageJson = corpsRequete.getJSONObject("donneesConnexion");
+                    final var homePage =
+                            new DMPThirdAuthenticationPage(pageJson).provideOTPCode(code);
+                    corpsReponse.put("donneesConnexion", homePage.toJSONObject());
                     break;
                 default:
                     throw new IllegalArgumentException();
             }
-        } catch (JSONException | NullPointerException | NoSuchElementException | IllegalArgumentException e) {
+        } catch (DMPAuthenticationException e) {
+            codeHttp = HttpStatus.CONFLICT;
+        } catch (JSONException | DMPTimeoutException | IllegalArgumentException e) {
             Utils.logErreur(e, logger);
             codeHttp = HttpStatus.BAD_REQUEST;
         } catch (Exception e) {
@@ -78,15 +64,8 @@ public final class Connexion {
         }
         return Commun.construireReponse(codeHttp, corpsReponse, request);
     }
-
-    private HttpStatus obtenirCodeHttp(ReponseConnexion resultat) {
-        switch (resultat.codeReponse) {
-            case ok: return HttpStatus.OK;
-            case erreurIds: return HttpStatus.CONFLICT;
-            default: return HttpStatus.INTERNAL_SERVER_ERROR;
-        }
-    }
 }
+
 
 class IncorrectIdsException extends Exception {
     private static final long serialVersionUID = 1L;
